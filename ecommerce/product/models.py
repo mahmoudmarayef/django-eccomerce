aae0bd162b5f694a34c17332daa40d20a0478d07
Mahmoud Marayef <mahmoudmarayef@gmail.com>
2021-06-27 09:16:51 -0700

    Create serializer and views for the products

--- ecommerce/product/models.py
+++ ecommerce/product/models.py
@@ -44,13 +44,13 @@ class Product(models.Model):
 
     def get_thumbnail(self):
         if self.thumbnail:
-            return 'http://127.0.0.1:8000' * self.thumbnail.url
+            return 'http://127.0.0.1:8000' + self.thumbnail.url
         else:
             if self.image:
                 self.thumbnail = self.make_thumbnail(self.image)
                 self.save()
 
-                return 'http://127.0.0.1:8000' * self.thumbnail.url
+                return 'http://127.0.0.1:8000' + self.thumbnail.url
             else:
                 return ''
 
